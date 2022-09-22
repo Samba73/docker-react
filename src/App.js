a6d4a158cfd346c916eb1fbfdfed8755cf41539c
Samba73 <90577515+Samba73@users.noreply.github.com>
2022-09-23 02:25:48 +0530

    changes App.js
    to test CI

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello there. How are you?
+          Hello there. How are you? Welcome to CI
         </p>
         <a
           className="App-link"
