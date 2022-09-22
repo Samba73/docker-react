e71a418d1525bb30b86c2f511da1df333a10d1bb
Samba73 <sambasivam.k@gmail.com>
2022-09-22 23:29:45 +0000

    change in feature branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello there. How are you? Welcome to CI, Travis
+          I'm now from new branch Feature
         </p>
         <a
           className="App-link"
